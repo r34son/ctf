95ade8d67f433cdf12d22d99857be9f6ae92f813
Yahub <seitasanov.yahub@gmail.com>
2022-05-08 18:01:28 +0300

    Feature: added task submit

--- ui/src/features/tasks/TaskDialog.tsx
+++ ui/src/features/tasks/TaskDialog.tsx
@@ -1,14 +1,19 @@
 import {
+  Box,
+  Chip,
   Dialog,
+  DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
   Slide,
+  Typography,
 } from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import draftToHtml from 'draftjs-to-html';
 import { forwardRef, ReactElement, Ref } from 'react';
+import { FlagForm } from './FlagForm';
 import { closeTask, openedTaskSelector } from './tasksSlice';
 
 const Transition = forwardRef(
@@ -33,7 +38,26 @@ export const TaskDialog = () => {
       onClose={onTaskClose}
       TransitionComponent={Transition}
     >
-      <DialogTitle>{task?.title}</DialogTitle>
+      <DialogTitle>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          flex={1}
+          overflow="hidden"
+        >
+          <Typography variant="h4" title={task?.title} noWrap>
+            {task?.title}
+          </Typography>
+          <Chip
+            label={`${task?.category} ${task?.points}`}
+            variant="outlined"
+            color="primary"
+            size="small"
+            sx={{ ml: 1 }}
+          />
+        </Box>
+      </DialogTitle>
       <DialogContent>
         {task?.description && (
           <DialogContentText
@@ -43,10 +67,9 @@ export const TaskDialog = () => {
           />
         )}
       </DialogContent>
-      {/* <DialogActions>
-        <Button onClick={handleClose}>Disagree</Button>
-        <Button onClick={handleClose}>Agree</Button>
-      </DialogActions> */}
+      <DialogActions>
+        <FlagForm />
+      </DialogActions>
     </Dialog>
   );
 };
